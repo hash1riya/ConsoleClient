f450df04aeecced96d07282122f114f0d40bb232
hash1riya <daniildrok1@gmail.com>
2024-08-01 13:12:41 +0300

    ex-1

---
+++ Model/Client.cs
@@ -0,0 +1,49 @@
+﻿using System.Diagnostics;
+using System.Net.Sockets;
+using System.Net;
+
+namespace ConsoleClient.Model;
+public class Client
+{
+    IPEndPoint endp;
+    Socket socket;
+    public byte[] Buffer = new byte[1024];
+
+    public Client(string ip, int port)
+    {
+        this.endp = new IPEndPoint(IPAddress.Parse(ip), port);
+        this.socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+    }
+
+    public bool IsConnected() => this.socket.Connected;
+    public void Connect()
+    {
+        try
+        {
+            socket.Connect(endp);
+        }
+        catch (SocketException ex)
+        {
+            Debug.WriteLine(ex.Message);
+        }
+    }
+
+    public void Disconnect()
+    {
+        try
+        {
+            this.socket.Shutdown(SocketShutdown.Both);
+            this.socket.Close();
+        }
+        catch (SocketException ex)
+        {
+            Debug.WriteLine(ex.Message);
+        }
+    }
+    public int Receive() => this.socket.Receive(this.Buffer);
+    public void Send(string strSend)
+    {
+        if (this.socket != null)
+            this.socket.Send(System.Text.Encoding.UTF8.GetBytes(strSend));
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,53 @@
-﻿namespace ConsoleClient;
+﻿using System.Diagnostics;
+
+namespace ConsoleClient;
 internal class Program
 {
     public static void Main(string[] args)
     {
+        string ip = "127.0.0.1";
+        int port = 1024;
+        Model.Client client = new Model.Client(ip, port);
 
+        string sendStr = "Hello server";
+        int bytesRead;
+        try
+        {
+            client.Connect();
+
+            if(client.IsConnected())
+                Console.WriteLine(
+                    $"[{DateTime.Now.ToString("dd/MM/yyyy HH:mm")}] " +
+                    $"SYSTEM: " +
+                    $"Connected!");
+
+            client.Send(sendStr);
+            Console.WriteLine(
+                $"[{DateTime.Now.ToString("dd/MM/yyyy HH:mm")}] " +
+                $"me: " +
+                $"{sendStr}");
+
+            bytesRead = client.Receive();
+            Console.WriteLine(
+                $"[{DateTime.Now.ToString("dd/MM/yyyy HH:mm")}] " +
+                $"server: " +
+                $"{System.Text.Encoding.UTF8.GetString(client.Buffer, 0, bytesRead)}");
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine(ex.Message);
+            Console.WriteLine(
+                $"[{DateTime.Now.ToString("dd/MM/yyyy HH:mm")}] " +
+                $"SYSTEM: " +
+                $"Remote host is unreacheble");
+        }
+        finally
+        {
+            Console.WriteLine(
+                $"[{DateTime.Now.ToString("dd/MM/yyyy HH:mm")}] " +
+                $"SYSTEM: " +
+                $"Disconnecting...");
+            client.Disconnect();
+        }
     }
 }
